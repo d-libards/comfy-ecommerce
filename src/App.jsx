9a37a9e9902c809e8364a3672c780debc9c87793
d-libards <dominiquelibardo09@gmail.com>
2024-11-04 05:36:23 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl font-bold underline bg-emerald-300">
+        Tailwind project
+      </h1>
+      <button className="btn">Button</button>
+    </>
+  );
+}
+
+export default App;
