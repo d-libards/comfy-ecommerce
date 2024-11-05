3c95331c5aee664759359a621a484c90b2ac0118
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-05 13:07:17 +0800

    carousel slick

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import hero1 from '../assets/hero1.webp';
 import hero2 from '../assets/hero2.webp';
@@ -6,6 +9,17 @@ import hero3 from '../assets/hero3.webp';
 import hero4 from '../assets/hero4.webp';
 
 const carouselImages = [hero1, hero2, hero3, hero4];
+
+const settings = {
+  dots: false,
+  infinite: true,
+  lazyLoad: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  pauseOnHover: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+};
 
 const Hero = () => {
   return (
@@ -21,20 +35,28 @@ const Hero = () => {
         </p>
         <div className="mt-10">
           <Link to="/products" className="btn btn-primary">
-            OUR PRODUCTS
+            SHOP NOW
           </Link>
         </div>
       </div>
-      <div className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box ">
-        {carouselImages.map((img) => {
-          return (
-            <div className="carousel-item">
-              <img src={img} className="rounded-box h-full w-80 object-cover" />
-            </div>
-          );
-        })}
+
+      {/* Slick Carousel */}
+      <div className="hidden lg:block lg:w-full bg-neutral rounded-box py-4 px-8">
+        <Slider {...settings}>
+          {carouselImages.map((img, index) => {
+            return (
+              <div className="flex justify-center px-1" key={index}>
+                <img
+                  src={img}
+                  className="rounded-box h-[30rem] w-full object-cover"
+                />
+              </div>
+            );
+          })}
+        </Slider>
       </div>
     </div>
   );
 };
+
 export default Hero;
